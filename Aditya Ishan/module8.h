9879e3e7c0f4b164400df29e4c105c13bbaeb51b
aditya Ishan <aditya.ishan27@gmail.com>
2019-04-03 12:43:27 +0530

    Update module8.h

--- Aditya Ishan/module8.h
+++ Aditya Ishan/module8.h
@@ -5,7 +5,7 @@
 class Module_8 : public Module_7{
 
    public:
-   void detect_Outliers_Rows_To_Remove();
-  	void remove_Outliers_Rows();
+      void detect_Outliers_Rows_To_Remove();
+      void remove_Outliers_Rows();
 };
 
