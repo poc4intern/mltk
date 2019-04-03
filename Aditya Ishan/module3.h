7ac0b9d8b31b47af3cda5568aef53dfb27afb8a8
aditya Ishan <aditya.ishan27@gmail.com>
2019-04-03 12:19:05 +0530

    Update module3.h

--- Aditya Ishan/module3.h
+++ Aditya Ishan/module3.h
@@ -5,10 +5,10 @@
 class Module_3 : public Module_2{
 	
 	public:
-		std::map<std::int32_t, std::string>                          max_Occur_Value_Of_Each_Column;
-		std::map<std::pair<std::int32_t, std::string>, std::int32_t> count_For_Each_Column;
+	   std::map<std::int32_t, std::string>                          max_Occur_Value_Of_Each_Column;
+           std::map<std::pair<std::int32_t, std::string>, std::int32_t> count_For_Each_Column;
 
-		void find_Count_Of_Each_Element_In_Categorical_Column();
-		void find_Max_Value_For_Each_Column();
-		void fill_Missing_Values_With_Mode();
+	   void find_Count_Of_Each_Element_In_Categorical_Column();
+	   void find_Max_Value_For_Each_Column();
+	   void fill_Missing_Values_With_Mode();
 };
