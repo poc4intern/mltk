f2cc52875b2a65fdccbccdacf65581b7f5499709
aditya Ishan <aditya.ishan27@gmail.com>
2019-04-03 12:20:29 +0530

    Update module4.h

--- Aditya Ishan/module4.h
+++ Aditya Ishan/module4.h
@@ -5,8 +5,8 @@
 class Module_4 : public Module_3{
 
    public:
-     std::map<std::int32_t, std::pair<long double, std::int32_t>>  total_Sum_And_Average_Of_Each_Column;
+      std::map<std::int32_t, std::pair<long double, std::int32_t>>  total_Sum_And_Average_Of_Each_Column;
 
-     void find_Mean_For_Each_Column();
-     void fill_Missing_Values_With_Mean();
+      void find_Mean_For_Each_Column();
+      void fill_Missing_Values_With_Mean();
 };
