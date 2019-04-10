6b73becba00a5ba503cbe013e3c768857ec48434
aditya Ishan <aditya.ishan27@gmail.com>
2019-04-10 15:09:57 +0530

    Update module5.h

--- Aditya Ishan/module5.h
+++ Aditya Ishan/module5.h
@@ -11,19 +11,19 @@ class Module_5 : public Module_4{
       std::map<std::int32_t, std::set<std::string>>                     unique_Elements_Of_Ordinal_Columns;
       std::vector<std::string>                                          nth_Column_Data;
       std::map<std::int32_t, std::int32_t>                              ordinal_Column_Indexes;
-   	std::map<std::int32_t, std::int32_t>                              nominal_Column_Indexes;
-   	std::vector<std::int32_t>                                         rank_Keeper;
-   	std::set<std::int32_t>                                            rows_To_Remove;
+      std::map<std::int32_t, std::int32_t>                              nominal_Column_Indexes;
+      std::vector<std::int32_t>                                         rank_Keeper;
+      std::set<std::int32_t>                                            rows_To_Remove;
       std::string                                                       error_Rank_Repeat = "Rank Already Assigned";
       std::string                                                       error_Rank_Not_Natural_Number = "Rank Should Be Greater Than Zero";
-   	
-   	void distinguish_Between_Ordinal_And_Nominal();
-   	void find_Unique_Values_For_The_Ordinal_Columns();
-   	void label_Encoding_Of_Ordinal_Columns();
-   	void replace_Ordinal_Columns_Value_With_Labels();
-   	void store_The_Values_Of_Nominal_Columns();
+
+      void distinguish_Between_Ordinal_And_Nominal();
+      void find_Unique_Values_For_The_Ordinal_Columns();
+      void label_Encoding_Of_Ordinal_Columns();
+      void replace_Ordinal_Columns_Value_With_Labels();
+      void store_The_Values_Of_Nominal_Columns();
       void remove_Nominal_Columns();
       void store_Nth_Column();
       void fill_Nominal_Values_With_Sparse_Matrix();
-};
+   };
 
